71a660b8d9ac109216e1e015fc7204e06c35227d
0508994 <m.n.milan@hotmail.com>
2018-11-26 23:07:04 +0100

    Testing grads - add norm checking.

---
+++ tests/test_grads.py
@@ -0,0 +1,19 @@
+# Debug
+import sys
+sys.path.append('../')
+from nn.sdnn import DNN
+from nn.iris import IrisDF
+from numpy.linalg import norm
+
+
+# http://ufldl.stanford.edu/wiki/index.php/Gradient_checking_and_advanced_optimization
+if __name__ == '__main__':   
+    df = IrisDF()
+    Xs = df.X_train #np.array([[0.68619022, 0.31670318, 0.61229281, 0.232249 ]])
+    ys = df.y_train #np.array([[1, 0, 0]])
+    nn = DNN(shape=[4, 6, 3], debug=False)
+    calc_grad = nn.objective(nn.get_params(), Xs, ys)[1]
+    num_grad = nn.compute_num_grads(Xs, ys)
+    print(calc_grad, end='\n\n')
+    print(num_grad, end='\n\n')
+    print(norm(calc_grad - num_grad) / (norm(calc_grad + num_grad))) # e-9 or less ! 
